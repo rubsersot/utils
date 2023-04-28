d8971cd1be1ded5e6521e5b8ba45db79eaf70f6c
rubsersot <121021895+rubsersot@users.noreply.github.com>
2023-04-28 09:23:44 +0200

    Update Utils.java

--- Utils.java
+++ Utils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UtilsExamenUF3;
+package utils;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
